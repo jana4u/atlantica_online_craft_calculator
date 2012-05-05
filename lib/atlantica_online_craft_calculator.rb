bf59855ffba159541cc3c00516ebc9f00ed2fdcf
jana4u <jana4u@seznam.cz>
2012-05-05 18:07:48 +0200

    generated new gem with bundler

---
+++ lib/atlantica_online_craft_calculator.rb
@@ -0,0 +1,5 @@
+require "atlantica_online_craft_calculator/version"
+
+module AtlanticaOnlineCraftCalculator
+  # Your code goes here...
+end
---
+++ lib/atlantica_online_craft_calculator/version.rb
@@ -0,0 +1,3 @@
+module AtlanticaOnlineCraftCalculator
+  VERSION = "0.0.1"
+end

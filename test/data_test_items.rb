d12090434357a1338105089b8413a23d73a597e1
jana4u <jana4u@seznam.cz>
2012-06-18 11:10:34 +0200

    added Music Sheet

--- test/data_test_items.rb
+++ test/data_test_items.rb
@@ -7,7 +7,7 @@ class DataTestItems < Test::Unit::TestCase
   end
 
   def test_lists_of_items
-    assert_equal 2221, AtlanticaOnlineCraftCalculator::Item.all.size
+    assert_equal 2231, AtlanticaOnlineCraftCalculator::Item.all.size
     assert AtlanticaOnlineCraftCalculator::Item.all.size > 0
     assert AtlanticaOnlineCraftCalculator::Item.ordered_items.size > 0
     assert_equal AtlanticaOnlineCraftCalculator::Item.all.size,
@@ -54,7 +54,7 @@ class DataTestItems < Test::Unit::TestCase
   end
 
   def test_item_skills
-    assert_equal 38, AtlanticaOnlineCraftCalculator::Item.ordered_item_skills.size
+    assert_equal 39, AtlanticaOnlineCraftCalculator::Item.ordered_item_skills.size
 
     items_size = 0
     AtlanticaOnlineCraftCalculator::Item.ordered_item_skills.each do |skill|
